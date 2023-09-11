4d5bbd4904928d003a07f63741237bce2a2871f3
omarr45 <58887202+omarr45@users.noreply.github.com>
2023-09-11 20:05:17 +0300

    upgrade to Astro 3.0 + change Layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import markdoc from "@astrojs/markdoc";
-import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
@@ -8,11 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  experimental: {
-    viewTransitions: true,
-    assets: true,
-  },
-  integrations: [mdx(), tailwind(), react(), markdoc()],
+  integrations: [tailwind(), react(), markdoc()],
   markdown: {
     shikiConfig: {
       theme: "dracula",
