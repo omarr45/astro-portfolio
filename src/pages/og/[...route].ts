48838b77a2e808c7bf2f9daacedeb3ffda828c66
omarr45 <58887202+omarr45@users.noreply.github.com>
2023-09-13 13:29:37 +0300

    added dynamic og-images

---
+++ src/pages/og/[...route].ts
@@ -0,0 +1,34 @@
+import { OGImageRoute } from "astro-og-canvas";
+import { getCollection } from "astro:content";
+
+const paths = await getCollection("blog");
+
+const pages = Object.fromEntries(
+  paths.map(({ id, slug, data }) => [id, { data, slug }]),
+);
+
+export const { getStaticPaths, GET } = OGImageRoute({
+  param: "route",
+  pages,
+  getImageOptions: async (_, { data }: (typeof pages)[string]) => {
+    return {
+      title: data.title,
+      description: data.date,
+      dir: data.isArabic ? "rtl" : "ltr",
+      logo: {
+        path: "./src/docs-logo.png",
+        size: [400],
+      },
+      border: { color: [255, 93, 1], width: 20, side: "inline-start" },
+      bgGradient: [
+        [42, 35, 62],
+        [23, 20, 36],
+      ],
+    };
+  },
+  //   (_path, page) => ({
+  //   title: "Omar AbdulRahman",
+  //   border: { width: 10 },
+  //   padding: 40,
+  // }),
+});
