37a4beacae692bbe29f74320e1bd58f0feb8f935
omar45casivu <omar.abdulrahman@casivu.com>
2024-05-02 10:53:40 +0300

    add new blog as draft

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  integrations: [tailwind(), react(), markdoc()],
+  integrations: [tailwind(), react(), markdoc({allowHTML:true})],
   markdown: {
     shikiConfig: {
       theme: "dracula",
